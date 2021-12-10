9a3c25ecc6cc5d756d32629ee4cfaeb7c378e883
Christopher Terrano <mterrano1@yahoo.com>
2021-12-10 13:18:43 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,29 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  initialize()
 });
+
+function renderOneToy(toy){
+  let card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img src='${toy.image}' class='toy-avatar'>
+    <p>${likes}</p>
+    <button class='like-btn' id=${toy.id}>'Like heart pic'</button>
+  `
+  console.log(card)
+}
+
+function getAllToys(){
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(toys.forEach(toy => renderOneToy(toy)))
+}
+
+function initialize(){
+  getAllToys()
+}
+
+initialize()
