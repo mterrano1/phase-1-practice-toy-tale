8c1fbbf2ddc6d083a65a2c5d107d55e7dee3a4a6
Christopher Terrano <mterrano1@yahoo.com>
2021-12-10 22:37:52 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
-  initialize()
 });
+
 
 function renderOneToy(toy){
   let card = document.createElement('div');
@@ -21,20 +21,35 @@ function renderOneToy(toy){
   card.innerHTML = `
     <h2>${toy.name}</h2>
     <img src='${toy.image}' class='toy-avatar'>
-    <p>${likes}</p>
+    <p>''</p>
     <button class='like-btn' id=${toy.id}>'Like heart pic'</button>
   `
-  console.log(card)
+document.querySelector('#toy-collection').appendChild(card)
 }
 
+const sendToy = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application.json",
+  },
+  body: JSON.stringify({
+    name: 'Baby Yoda',
+    image: 'https://target.scene7.com/is/image/Target/GUEST_c9596f8d-70ad-4700-af78-39b40fd95e09?wid=488&hei=488&fmt=pjpeg',
+    likes: 0
+  }),
+};
+
 function getAllToys(){
-  fetch('http://localhost:3000/toys')
+  fetch('http://localhost:3000/toys', sendToy)
   .then(res => res.json())
-  .then(toys.forEach(toy => renderOneToy(toy)))
+  // .then(toys => toys.forEach(toy => renderOneToy(toy)))
+  .then(toys => renderOneToy(toys))
 }
 
 function initialize(){
   getAllToys()
 }
 
-initialize()
+initialize();
+
